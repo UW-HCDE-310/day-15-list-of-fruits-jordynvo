6a572f413d9f10190e31c6fa5fbf6aa581948ed2
Jordyn Vo <jordynvo@Jordyns-MacBook-Pro-8.local>
2024-11-20 21:10:58 -0800

    Set up flask app with a fruit filter and fake API key integration

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import keys
 
 app = Flask(__name__)
 @app.route("/")
@@ -8,4 +9,12 @@ def index():
             {"name": "oranges", "quantity": 2},
             {"name": "strawberries", "quantity": 6}
     ]
-    return render_template("index.html", fruits=fruits)
+    filtered_fruits = [fruit for fruit in fruits if fruit["name"].startswith("o") and fruit["quantity"] < 3]
+
+    print(keys.MY_SECRET_API_KEY_1)
+    print(keys.MY_SECRET_API_KEY_2)
+
+    return render_template("index.html", fruits=filtered_fruits)
+
+if __name__ == "__main__":
+    app.run(debug=True)
